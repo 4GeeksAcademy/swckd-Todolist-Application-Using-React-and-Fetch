cef1b40eafe83dc43335c76de3e55705f31e424c
Alejandro <aleckbeu@gmail.com>
2024-04-10 19:04:00 +0000

    Alejandro

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	// const [todo, setTodo] = useState({});
+	const [todos, setTodos] = useState([]);
+
+	const [inputValue, setInputValue] = useState("");
+
+	const handleInputValue = (event) => {
+		setInputValue(event.target.value);
+	}
+
+	const handleSubmit = (event, inputValue) => {
+		if (event.key === "Enter") {
+			// setTodoList(todoList.concat(inputValue));
+			postTodo(inputValue);
+			setInputValue("");
+		}
+	}
+
+	const postTodo = async (tarea) => {
+		try {
+
+			const myHeaders = new Headers();
+			myHeaders.append("Content-Type", "application/json");
+
+			const raw = JSON.stringify({
+				"label": tarea,
+				"is_done": false
+			});
+
+			const requestOptions = {
+				method: "POST",
+				headers: myHeaders,
+				body: raw,
+				redirect: "follow"
+			};
+
+			const response = await fetch("https://playground.4geeks.com/todo/todos/alejandro", requestOptions)
+			const result = await response.text();
+
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch("https://playground.4geeks.com/todo/users/alejandro");
+				const data = await response.json();
+
+				setTodos(data.todos);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchData();
+	}, []);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+		< div className="d-flex flex-column justify-content-center align-items-center text-center w-50 m-auto" >
+
+			<h1 className="text-center mt-5">lista de tareas</h1>
+			<ul className="list-group list-group-flush">
+				<li className="list-group-item">
+					<input type="text" className="border-0 text-center" placeholder="Añade una tarea"
+						value={inputValue}
+						onChange={handleInputValue}
+						onKeyDown={handleSubmit}
+					/>
+				</li>
+				{!todos ? <li className="list-group-item">No hay tareas pendientes</li> : ""}
+
+				{todos && todos.map((item, index) => (
+					<li className="list-group-item text-start" key={index}>
+						<span className="me-2 badge text-bg-secondary">X</span>
+						{item.label}
+					</li>
+				))}
+
+			</ul>
+			{todos ? <p>Tareas pendientes:  {todos.length}</p> : ""}
+
+		</div >
 	);
 };
 
